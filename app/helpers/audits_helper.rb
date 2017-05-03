8def68aaef9875bcd19823abf8360a2968b3ff47
oglobin <ginc.xyz@gmail.com>
2017-05-03 12:55:21 +0300

    f

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,2 +1,16 @@
 module AuditsHelper
+  def get_record(id, type)
+    if type == 'Budget' || type == 'ConstructorObject'
+      if type == 'Budget'
+        Estimate.with_deleted.find(id) if Estimate.with_deleted.exists?(id: id)
+        Solution.with_deleted.find(id) if Solution.with_deleted.exists?(id: id)
+      else
+        Primitive.with_deleted.find(id) if Primitive.with_deleted.exists?(id: id)
+        Composite.with_deleted.find(id) if Composite.with_deleted.exists?(id: id)
+      end
+    else
+      clazz = type.constantize
+      clazz.with_deleted.find(id)
+    end
+  end
 end
