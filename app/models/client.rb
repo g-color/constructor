2337580119aea1005e9fa8082cb2fb68e6e53754
g1nc <ginc.xyz@gmail.com>
2017-06-08 20:37:22 +0300

    share client to other users

--- app/models/client.rb
+++ app/models/client.rb
@@ -25,6 +25,7 @@ class Client < ApplicationRecord
   end
 
   def share(users)
+    users = JSON.parse(users)
     UserClient.where(client: self, owner: false).where.not(user_id: users).destroy_all
     users.each do |user|
       UserClient.create(client: self, user_id: user, owner: false) unless shared_to_user?(user)
