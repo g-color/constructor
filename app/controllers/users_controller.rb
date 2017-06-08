8324fc4c8d594800142947a7e34304cec6ae788a
oglobin <ginc.xyz@gmail.com>
2017-06-08 11:57:15 +0300

    order to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.order_by(:last_name, :first_name)
     if params[:name].present?
       @users = @users.select do |m|
         m.full_name.downcase.include? params[:name].downcase
