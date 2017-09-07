fb8979ec7e966a4b763eb4761bef79729fe9c9ed
Oleg Globin <ginc.xyz@gmail.com>
2017-09-07 19:54:11 +0300

    fix min floor height

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -28,9 +28,9 @@ class Budget < ApplicationRecord
   validates :area,               float_with_precision_two: true
   validates :first_floor_height, presence: true, numericality: { greater_than: 0 }, float_with_precision_three: true
 
-  validates :second_floor_height_min, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }, float_with_precision_three: true
+  validates :second_floor_height_min, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal: :second_floor_height_max }, float_with_precision_three: true
   validates :second_floor_height_max, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }, float_with_precision_three: true
-  validates :third_floor_height_min, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }, float_with_precision_three: true
+  validates :third_floor_height_min, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal: :third_floor_height_max }, float_with_precision_three: true
   validates :third_floor_height_max, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }, float_with_precision_three: true
 
   belongs_to :solution
