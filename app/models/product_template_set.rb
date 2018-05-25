0cd9a1cd64ab0a74b7438f7d258fecdf1d539100
Oleg Globin <ginc.xyz@gmail.com>
2018-05-25 18:42:05 +0300

    fixes

--- app/models/product_template_set.rb
+++ app/models/product_template_set.rb
@@ -1,8 +1,6 @@
 class ProductTemplateSet < ApplicationRecord
   acts_as_paranoid
 
-  default_scope { order(id: :asc) }
-
   belongs_to :product
   belongs_to :product_template
   belongs_to :product_set
